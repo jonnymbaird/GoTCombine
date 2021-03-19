4243672d7265a5f42cfc2b27d533a1ecc8c9c134
Jonny Baird <jb@jonathanbaird.co.uk>
2021-03-19 17:02:40 +0000

    fixed linting and launchscreen

--- GoTCombine/AppDelegate.swift
+++ GoTCombine/AppDelegate.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 import TinyConstraints
-
+// swiftlint:disable all
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-// swiftlint:disable all
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
